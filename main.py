4ea3616f34662bd1aa72386e773882c7c73d2cc4
Igor Ferreira <igor-freitas10@hotmail.com>
2022-09-12 10:42:20 -0300

    Handle quiz interruption

--- main.py
+++ main.py
@@ -3,13 +3,25 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
-question_bank = [Question(question["question"], question["correct_answer"]) for question in question_data]
-shuffle(question_bank)
 
-quiz = QuizBrain(question_bank)
+def main() -> None:
+    question_bank = [Question(question["question"], question["correct_answer"]) for question in question_data]
+    shuffle(question_bank)
 
-while quiz.still_has_questions():
-    quiz.next_question()
+    quiz = QuizBrain(question_bank)
 
-print(f"You complete the quiz.\n"
-      f"Your final score is: {quiz.score}/{quiz.question_number}")
+    while quiz.still_has_questions():
+        try:
+            quiz.next_question()
+        except KeyboardInterrupt:
+            quiz.question_number -= 1
+            print(f"\n\nYou left the quiz with {len(question_bank) - quiz.question_number} questions missing.")
+            break
+    else:
+        print(f"You complete the quiz.")
+
+    print(f"Your final score is: {quiz.score}/{quiz.question_number}")
+
+
+if __name__ == "__main__":
+    main()
